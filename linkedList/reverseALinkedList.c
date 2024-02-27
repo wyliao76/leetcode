520ae70b1fde70f812c7c86113640ff6307889f8
Will Liao <will.liao1@hp.com>
2024-02-26 21:39:07 -0800

    add reverse linkedList

---
+++ linkedList/reverseALinkedList.c
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct ListNode
+{
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode *createLinkedLists(int *arr, int size)
+{
+    struct ListNode *head = NULL;
+    struct ListNode *prev = NULL;
+    for (int i = 0; i < size; i++)
+    {
+        struct ListNode *temp = (struct ListNode*) malloc(sizeof(struct ListNode));
+        // printf("%d\n", arr[i]);
+        temp->val = arr[i];
+        temp->next = NULL;
+        if (!head)
+        {
+            head = temp;
+        } else
+        {
+            prev->next = temp;
+        }
+        prev = temp;
+    }
+    return head;
+}
+
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode *prev = NULL;
+    
+    while(head != NULL)
+    {
+        struct ListNode *temp = (struct ListNode*) malloc(sizeof(struct ListNode));
+        temp->val = head->val;
+        temp->next = prev;
+        prev = temp;
+        head = head->next;
+    }
+    return prev;
+}
+
+int main()
+{
+    int arr[] = {1,2,3,4,5};
+    // int arr[] = {1, 2};
+    // int arr[] = {};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    struct ListNode *l = createLinkedLists(arr, size);
+    struct ListNode *reversed;
+    reversed = reverseList(l);
+    while(reversed != NULL)
+    {
+        printf("%d\n", reversed->val);
+        reversed = reversed->next;
+    }
+    return 0;
+}
