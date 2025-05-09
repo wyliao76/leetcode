299dc2ce08912cf89bd1bd6e8293ea3384718075
Will Liao <wyliao76@gmail.com>
2025-05-09 13:41:33 -0700

    add O(n) solution

--- string/longestSubstringWithoutRepeatingCharacters.test.js
+++ string/longestSubstringWithoutRepeatingCharacters.test.js
@@ -1,37 +1,44 @@
-/**
+// /** this is O(n^2) though
+//  * @param {string} s
+//  * @return {number}
+//  */
+// const lengthOfLongestSubstring = function(s) {
+//     let max = 0
+//     const substring = []
+
+//     for (let i = 0; i < s.length; i++) {
+//         if (substring.includes(s[i])) {
+//             const index = substring.indexOf(s[i]) + 1
+//             for (let i = 0; i < index; i++) {
+//                 substring.shift()
+//             }
+//         }
+//         substring.push(s[i])
+//         if (substring.length > max) {
+//             max = substring.length
+//         }
+//     }
+//     return max
+// }
+
+/** this is O(n)
  * @param {string} s
  * @return {number}
  */
 const lengthOfLongestSubstring = function(s) {
+    let left = 0
     let max = 0
-    const substring = []
+    const dict = {}
 
     for (let i = 0; i < s.length; i++) {
-        if (substring.includes(s[i])) {
-            const index = substring.indexOf(s[i]) + 1
-            for (let i = 0; i < index; i++) {
-                substring.shift()
-            }
+        if (dict[s[i]] !== undefined) {
+            // otherwise new left might be smaller than current left
+            left = Math.max(dict[s[i]] + 1, left)
         }
-        substring.push(s[i])
-        if (substring.length > max) {
-            max = substring.length
-        }
+        dict[s[i]] = i
+        max = Math.max(i - left + 1, max)
     }
     return max
-
-    // let max = 0
-    // const dict = {}
-    // let left = 0
-
-    // for (let i = 0; i < s.length; i++) {
-    //     if (dict[s[i]]) {
-    //         left = dict[s[i]] > left ? dict[s[i]] : left
-    //     }
-    //     max = (i - left + 1) > max ? (i - left + 1) : max
-    //     dict[s[i]] = i + 1
-    // }
-    // return max
 }
 
 describe('playground', () => {
